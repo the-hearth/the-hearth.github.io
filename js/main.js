c7bc63d4dd483004bef13a73e95a74bddbc4ea07
the-hearth <35165509+the-hearth@users.noreply.github.com>
2018-01-31 16:56:09 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -68,12 +68,12 @@ $(document).ready(function() {
 		//for scrolling downwards
 		if(position<$("#panel-content").scrollTop()){
 			position = $("#panel-content").scrollTop();
-			if(position >= cum_panelheight - frame_height-5){
+			if(position >= cum_panelheight - frame_height -2){
 				//alert('this panel is over: '+ $(current_panel).attr('id'));
 				$('.panel-background').find('img:visible').fadeOut(500);
 				//scroll to next, update counters, change background
 				if($(current_panel).next().length>0){
-					$('#panel-content').animate({ scrollTop: cum_panelheight-5 }, 1000);
+					$('#panel-content').animate({ scrollTop: cum_panelheight +6 }, 1000);
 					current_panel = $(current_panel).next();
 					//alert('current panel is now: '+ $(current_panel).attr('id'));
 					$('.panel-background').find('#'+$(current_panel).attr('id')).fadeIn(500);
@@ -93,13 +93,52 @@ $(document).ready(function() {
 					//alert('current panel is now: '+ $(current_panel).attr('id'));
 					cum_panelheight = $(current_panel).outerHeight();
 					$('.panel-background').find('#'+$(current_panel).attr('id')).fadeIn(500);
-					$('#panel-content').animate({ scrollTop: 0 }, 1000);
+					//try not to trigger going back again!
+					$('#panel-content').animate({ scrollTop: 6 }, 1000);
 				}
 				//update position
 				position = $("#panel-content").scrollTop();
 			}
 		}
-
+		//for upwards scroll
+		else {
+			position = $("#panel-content").scrollTop();
+			if(position < cum_panelheight - $(current_panel).innerHeight() +5){
+				//alert('this panel is scrolled up: '+ $(current_panel).attr('id'));
+				$('.panel-background').find('img:visible').fadeOut(500);
+				//scroll to previous, update counters, change background
+				if($(current_panel).prev().length>0){
+					cum_panelheight -= $(current_panel).outerHeight();
+					current_panel = $(current_panel).prev();
+					$('.panel-background').find('#'+$(current_panel).attr('id')).fadeIn(500);
+					$('#panel-content').animate({ scrollTop: cum_panelheight - frame_height }, 1000);
+					//alert('current panel is now: '+ $(current_panel).attr('id'));
+				}
+				//load previous page, change background, do nothing if it's the top of the first page
+				else {
+					$(current_page).fadeOut(500);
+					if($(current_page).prev().length>0){
+						current_page = $(current_page).prev();
+					}
+					else {
+						current_page = $("#panel-content").children('article:last');
+					}
+					$(current_page).fadeIn(1000);
+					//get first panel
+					current_panel = $(current_page).children('div:last');
+					$('.panel-background').find('#'+$(current_panel).attr('id')).fadeIn(500);
+					//alert('current panel is now: '+ $(current_panel).attr('id'));
+					cum_panelheight=0;
+					$(current_page).children('div').each(function(index,element){
+						cum_panelheight += $(element).outerHeight();
+					});
+					//try not to trigger skipping front again!
+					$('#panel-content').animate({ scrollTop: cum_panelheight - frame_height -6 }, 1000);
+				}
+				//update position
+				position = $("#panel-content").scrollTop();
+			}
+		}
 	});
 
 });//end of document-ready code
